757af0c7d2ba7bfe308ce5fb492b7ac5d4c8a5cc
chinzhilla221564-boop <chinzhilla221564@gmail.com>
2026-05-21 18:10:28 +0500

    Make CORS more permissive for testing

--- backend/app.py
+++ backend/app.py
@@ -28,12 +28,14 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:5500",
         "http://localhost:8000",
-        "http://127.0.0.1:8000"
+        "http://127.0.0.1:8000",
+        "http://localhost:3000",
+        "*"
     ],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=["Content-Type", "Authorization", "Accept", "Origin"],
-    expose_headers=["Content-Type"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 
